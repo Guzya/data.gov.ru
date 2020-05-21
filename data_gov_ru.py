e7bc403f636ff926382c7700173d9a23c8bbbb12
Guzya <ma41h@yandex.ru>
2020-05-21 10:15:57 +0300

    Добавил простой логгер

--- data_gov_ru.py
+++ data_gov_ru.py
@@ -16,6 +16,8 @@ import time
 import random
 import os
 import sys
+import csv
+import logging
 
 def getDatasets(access_token, ctx):
     """Перечень наборов данных"""
@@ -23,13 +25,13 @@ def getDatasets(access_token, ctx):
     try:        
         html = urlopen('https://data.gov.ru/api/json/dataset/?access_token=' + access_token, context=ctx)
         return json.loads(html.read(), strict=False)
-    except HTTPError as e:
+    logger.error HTTPError as e:
         print(e)
         return None
-    except URLError as e:
+    logger.error URLError as e:
         print(e)
         return None
-    except JSONDecodeError as e:
+    logger.error JSONDecodeError as e:
         print(e)
         return None
 
@@ -38,25 +40,25 @@ def getDatasetVersion(dataset, access_token, ctx=None):
     """Достаем информацию по конкретному набору данных (НЕ сами данные)"""
 
     try:		
-        print('https://data.gov.ru/api/json/dataset/' + dataset + '?access_token=' + access_token)
+        logger.info('https://data.gov.ru/api/json/dataset/' + dataset + '?access_token=' + access_token)
         html = urlopen('https://data.gov.ru/api/json/dataset/' + dataset + '?access_token=' + access_token, context=ctx)
         return json.loads(html.read(), strict=False)
     except HTTPError as e:
-        print(e)
+        logger.error(e)
         return None
     except URLError as e:
-        print(e)
+        logger.error(e)
         return None
     except JSONDecodeError as e:
         with open(os.path.dirname(sys.argv[0]) + '/err_json.txt','w') as f:
             f.write(html.read())
-        print(e)
+        logger.error(e)
         return None
     except Exception as e:
         with open(os.path.dirname(sys.argv[0]) + '/err.txt','w') as f:
             f.write(html.read())
-        print(e)
-        print(dataset)
+        logger.error(e)
+        logger.error(dataset)
         return None
         
         
@@ -76,19 +78,19 @@ def getDatasetData(dataset, access_token, ctx=None):
         else:
             fileName = creatorDir + "/" + dataset['identifier'] + '.HZ'
             fileNameContent = creatorDir + "/" + dataset['identifier'] + "_structure.HZ"
-            print('DataSet  {} : {}'.format(dataset['identifier'], dataset['format']))
+            logger.info('DataSet  {} : {}'.format(dataset['identifier'], dataset['format']))
 
         # Получаем временную отметку для следующего запроса
         html = urlopen('https://data.gov.ru/api/json/dataset/' + dataset[
             'identifier'] + '/version/' + '?access_token=' + access_token, context=ctx)
-        print('Url Version: ' + html.url)
+        logger.info('Url Version: ' + html.url)
         datasetCreated = json.loads(html.read())
 
         # Получаем ссылки на файлы с данными
         html = urlopen(
             'https://data.gov.ru/api/json/dataset/' + dataset['identifier'] + '/version/' + datasetCreated[0][
                 'created'] + '/?access_token=' + access_token, context=ctx)
-        print('Url Sources: ' + html.url)
+        logger.info('Url Sources: ' + html.url)
 
         datasetSource = json.loads(html.read())
 
@@ -100,19 +102,37 @@ def getDatasetData(dataset, access_token, ctx=None):
                 updateDate = datetime.datetime.strptime(line['updated'], '%Y%m%dT%H%M%S')
                 index = datasetSource.index(line)
 
-        print('updateDate: {} ,Index: {}'.format(updateDate, index))
+        logger.info('updateDate: {} ,Index: {}'.format(updateDate, index))
 
-       # Качаем файл с данными        
-        dataFile = urlopen(datasetSource[index]['source'], context=ctx)
-        print('URL dataset file: ' + datasetSource[index]['source'])
-                	        
-        with open(fileName, 'bw') as f:
-            f.write(dataFile.read())
+
+       # Качаем файл с данными 
+        if dataset['format'] == 'csv':
+            dataFile = urlopen(
+            'https://data.gov.ru/api/json/dataset/' + dataset['identifier'] + '/version/' + datasetCreated[0][
+                'created'] + '/content/?access_token=' + access_token, context=ctx)
+            logger.info('URL dataset file: ' + 'https://data.gov.ru/api/json/dataset/' + dataset['identifier'] + '/version/' + datasetCreated[0][
+                'created'] + '/content/?access_token=' + access_token)
+            
+            with open(fileName, 'w') as f:
+                dataLines = json.loads(dataFile.read())
+                w = csv.DictWriter(f, dataLines[0].keys(),delimiter=';')
+                w.writeheader()
+                for line in dataLines:
+                    w.writerow(line)
+            
+        else:        
+            dataFile = urlopen(datasetSource[index]['source'], context=ctx)
+            logger.info('URL dataset file: ' + datasetSource[index]['source'])
+            with open(fileName, 'bw') as f:
+                f.write(dataFile.read())
 
         html = urlopen(
             'https://data.gov.ru/api/json/dataset/' + dataset['identifier'] + '/version/' + datasetCreated[0][
                 'created'] + '/structure' + '/?access_token=' + access_token, context=ctx)
-
+        
+        logger.info('URL structure file: ' + 'https://data.gov.ru/api/json/dataset/' + dataset['identifier'] + '/version/' + datasetCreated[0][
+                'created'] + '/structure' + '/?access_token=' + access_token)
+        
         datasetStructure = json.loads(html.read())
 
         dataFileStructure = urlopen(datasetStructure['source'], context=ctx)
@@ -121,18 +141,18 @@ def getDatasetData(dataset, access_token, ctx=None):
             f.write(dataFileStructure.read())
 
     except HTTPError as e:
-        print(e)
+        logger.error(e)
         return None
     except URLError as e:
-        print(e)
+        logger.error(e)
         return None
     except JSONDecodeError as e:
-        print(e)
+        logger.error(e)
         return None        
     except Exception as e:
-        print('Ошибка -------:')
-        print(e)
-        print(dataset)
+        logger.error('Ошибка -------:')
+        logger.error(e)
+        logger.error(dataset)
         return None
 
 
@@ -153,9 +173,9 @@ def main(access_token, search_string=None, ctx=None):
 
     datasets = getDatasets(access_token, ctx)
     if datasets is None:
-        print('Не смогли получить перечень наборов данных')
+        logger.error('Не смогли получить перечень наборов данных')
         exit(1)
-    print('Всего наборов данных: {}'.format(len(datasets)))
+    logger.info('Всего наборов данных: {}'.format(len(datasets)))
     
     with open('datasets.json','w', encoding='utf-8') as f:
         json.dump(datasets, f, indent=4)
@@ -164,35 +184,60 @@ def main(access_token, search_string=None, ctx=None):
         exit(0)
 		
     datasets_data = getDataSetsFiltered(datasets, search_string.lower())        # Отбираем данные по Строке поиска
-    print('По критерию поиска подходит наборов: {}'.format(len(datasets_data)))
+    logger.info('По критерию поиска подходит наборов: {}'.format(len(datasets_data)))
     
     datasets_version = []
     i = 0
     for dataset in datasets_data:
         i = i + 1
-        print('getDatasetVersion: ' + str(i))
+        logger.info('getDatasetVersion: ' + str(i))
         time.sleep(random.randint(0, 6))
         datasets_version.append(getDatasetVersion(dataset['identifier'], access_token, ctx))
     
     i = 0
     for dataset in datasets_version:
         i = i + 1
-        print('getDatasetData: ' + str(i))
+        logger.info('getDatasetData: ' + str(i))
         time.sleep(random.randint(0, 6))
         getDatasetData(dataset, access_token, ctx)
 
 
 if __name__ == '__main__':
-
+	
+	
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+	
+    formatLogger = logging.Formatter('%(asctime)s: %(name)-12s: %(funcName)-12s: %(levelname)-8s: %(message)s')
+    formatConsole = logging.Formatter('%(asctime)s: %(levelname)-6s: %(message)s')
+	
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(formatConsole)
+	
+    filehandler = logging.FileHandler('Data.gov.ru.log')
+    filehandler.setLevel(logging.INFO)
+    filehandler.setFormatter(formatLogger)
+	
+    logger.addHandler(console)
+    logger.addHandler(filehandler)
+	
+    logger.info('Начало работы ------------------------------------- ')	
+    startTime = datetime.datetime.now()
+    
     ctx = ssl.create_default_context(capath='/etc/ssl/certs')
     
     access_token = os.environ['data_gov_access_token']  # Выдается при регистрации на data.gov.ru   
     if len(sys.argv) == 1:
         main(access_token, ctx=ctx)
     elif len(sys.argv) == 2:
-        print('Строка поиска: \'{}\''.format(sys.argv[1]))
+        logger.info('Строка поиска: \'{}\''.format(sys.argv[1]))
         main(access_token, search_string=sys.argv[1], ctx=ctx)
     else:
-        print('Аргументов должно быть не больше одного.')
-        print('Пример: python3 data_gov_ru.py \'Москв\'')
+        logger.error('Аргументов должно быть не больше одного.')
+        logger.error('Пример: python3 data_gov_ru.py \'Москв\'')
         exit(1)
+        
+    stopTime = datetime.datetime.now()    
+    logger.info('Времы выполнения скрипта: ' + str(stopTime - startTime))
+    logger.info('Окончание работы ------------------------------------- ' )
